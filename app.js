7011b42b1e15c48ff5d55ec4aeedc08e3783cff7
myname <vs20012001vs@gmail.com>
2022-05-14 00:26:59 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const req = require("express/lib/request");
+
+const app =express();
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+//Request targetting all articles
+
+app.route("/articles")
+
+.get(function(req,res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        }
+        else{
+            res.send(err);
+        }
+    });
+})
+
+.post(function(req,res){
+    
+    const newArticle = new Article({
+        title:req.body.title,
+        content:req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Successfully added a new article");
+        }
+        else{
+            res.send(err);
+        }
+    });
+})
+
+.delete(function(req,res){
+    Article.deleteMany({},function(err){
+        if(!err){
+            res.send("Deleted Everything");
+        }
+        else{
+            res.send(err);
+        }
+    });   
+});
+
+//Request Targeting specific article
+
+app.route("/articles/:articleTitle")
+
+.get(function(req,res){
+    Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+        }
+        else{
+            res.send("Noting Found");
+        }
+    });
+})
+
+.put(function(req,res){
+    Article.findOneAndReplace(
+        {title:req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {new:true},
+        function(err){
+            if(!err){
+                res.send("Successfully replaced article");
+            }
+        }); 
+})
+
+.patch(function(req,res){
+    Article.findOneAndUpdate(
+        {title:req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article");
+            }
+        }); 
+
+})
+
+.delete(function(req,res){
+    Article.deleteOne({title:req.params.articleTitle},function(err){
+        if(!err){
+            res.send("Succesfully Deleted the Article");
+        }
+        else{
+            res.send(err)
+        }
+    });
+});
+
+
+app.listen(3000,function(){
+    console.log("Wiki server started on port 3000");
+});
